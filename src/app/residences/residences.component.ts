95186504baf1ee0200ba12c6ee9ff71660e1fd17
syrine0219 <syrine.benamara@esprit.tn>
2025-02-12 20:45:27 +0100

    workshop1

--- src/app/residences/residences.component.ts
+++ src/app/residences/residences.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Residence } from '../Core/models/residence';
+
+
+
+
 
 @Component({
   selector: 'app-residences',
   templateUrl: './residences.component.html',
+  
   styleUrls: ['./residences.component.css']
+
 })
 export class ResidencesComponent {
+  listResidences: Residence[] = [
+    { id: 1, name: "El fel", address: "Borj Cedria", image: "../../assets/images/R1.jpg", status: "Disponible" },
+    { id: 2, name: "El yasmine", address: "Ezzahra", image: "../../assets/images/R2.jpg", status: "Disponible" },
+    { id: 3, name: "El Arij", address: "Rades", image: "../../assets/images/R3.jpg", status: "Vendu" },
+    { id: 4, name: "El Anber", address: "inconnu", image: "../../assets/images/R4.jpg", status: "En Construction" }
+  ];
 
+  showLocation(address: string) {
+    if (address === "inconnu") {
+      alert("L'adresse de cette résidence est inconnue");
+    } else {
+      alert("Adresse: " + address);
+    }
+  }
+  favorites: Residence[] = [];
+
+addToFavorites(residence: Residence) {
+  this.favorites.push(residence);
+  alert(residence.name + " ajouté aux favoris !");
+}
+
+
+
+
+
+  
 }
