49aaf81bd79327dac6946417fbe706c478f03ead
syrine0219 <syrine.benamara@esprit.tn>
2025-02-12 19:50:43 +0100

    Initial commit

---
+++ src/app/residences/residences.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-residences',
+  templateUrl: './residences.component.html',
+  styleUrls: ['./residences.component.css']
+})
+export class ResidencesComponent {
+
+}
